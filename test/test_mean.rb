6a91926baeb511c4ade2dd90f0e081159f43b6f9
Ralf Mueller <ralf.mueller@dkrz.de>
2020-08-18 18:17:41 +0200

    avoid working with side effects in test setups

--- test/test_mean.rb
+++ test/test_mean.rb
@@ -4,6 +4,7 @@ require 'minitest/autorun'
 
 class MeanTest < Minitest::Test
 
+  # helper method to split array into equally sizes parts
   def splitArray(size, splitCount)
     indexSplit = []
     (0...splitCount).each {|s|
@@ -12,7 +13,9 @@ class MeanTest < Minitest::Test
       }
     }
 
-    (1..(splitCount-1)).to_a.reverse.each {|i| indexSplit[i] = indexSplit[i]-indexSplit[i-1]}
+    (1..(splitCount-1)).to_a.reverse.each {|i|
+      indexSplit[i] = indexSplit[i]-indexSplit[i-1]
+    }
 
     indexSplit
   end
@@ -24,16 +27,35 @@ class MeanTest < Minitest::Test
     result = splitArray(values.size,4)
     assert_equal(result, expectedResult)
   end
+
+
+  # methods for creating test data, test weights and some more options to tuning
+  #   size: amount random data values
+  #   weight: basic weight for later averaging
+  #   scale: list of scale factors for the list of weights, i.e. the list of
+  #   weights it splitten into equally sized parts and each of them is
+  #   multiplied with the factor from the argument list
+  #   list: if this is given, it overwrites the random values for the data set
   def setValues(size, weight, scale: [], list: [])
-    @@testSet     = Array.new(size) {rand**Math.log(rand).abs*1000}
-    @@testSet     = list.empty? ? Array.new(size) {rand}  : list
-    @@testWeights = Array.new(size) {weight}
+    testSet     = Array.new(size) {rand**Math.log(rand).abs*1000}
+    testSet     = list.empty? ? Array.new(size) {rand}  : list
+
+    testWeights = Array.new(size) {weight}
     unless scale.empty? then
       splittedIndices = splitArray(size, scale.size)
-      splittedIndices.each_with_index {|is,si| is.each {|i| @@testWeights[i] = scale[si]*@@testWeights[i]}}
+      splittedIndices.each_with_index {|is,si|
+        is.each {|i| 
+          testWeights[i] = scale[si]*testWeights[i]
+        }
+      }
     end
-    @@expectedMean = MeanProcesing::arith_jfe(@@testSet)
+
+    expectedMean = MeanProcesing::arith_jfe(testSet)
+
+    [testSet, testWeights, expectedMean]
   end
+
+  # setup variation for testing the different averaging algorhithms
   def setupSmall
     @@testSet = (0..9).to_a
     @@expectedMean = 4.5
@@ -41,24 +63,24 @@ class MeanTest < Minitest::Test
     @@expectedRunMean = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]
   end
   def setupSmallWeighted
-    @@expectedMean = 4.5
-    setValues(10,1.0,scale: [0.5,1,2],list: (0..9).to_a)
+    @@testSet, @@testWeights, @@expectedMean = setValues(10,1.0,scale: [0.5,1,2],list: (0..9).to_a)
+    assert_equal(@@expectedMean, 4.5)
   end
   def setupMedium
     size = 720
     weight = 2
-    setValues(size,weight)
+    @@testSet, @@testWeights, @@expectedMean = setValues(size,weight)
   end
   def setupMediumDifferentWeights
     size = 720
     weight = 1
     weight_scale = [2,1,0.5]
-    setValues(size,weight,weight_scale)
+    @@testSet, @@testWeights, @@expectedMean = setValues(size,weight,weight_scale)
   end
   def setupLarge
     size = 100000
     weight = 1800
-    setValues(size,weight)
+    @@testSet, @@testWeights, @@expectedMean = setValues(size,weight)
   end
 
   def setup
